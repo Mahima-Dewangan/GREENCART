7f9a4b38ddb6541229c2594bf35e03f6fc956dbe
Mahima-Dewangan <mahimadewangan11.com>
2025-08-15 00:01:54 +0530

    main banner created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,8 +8,10 @@ const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
   return (
     <div>
-      <Navbar/>
-      <div className={`px-6 md:px-16 lg:px-24 xl:px-32`}>
+
+     {isSellerPath ? null : <Navbar/> } 
+
+      <div className={`${isSellerPath ? " " : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
           <Route path='/' element={<Home/>}/>
         </Routes>
