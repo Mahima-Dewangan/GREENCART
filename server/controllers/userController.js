d8aa6ec7d9c0e8fc11452f8432f64e3b8a3507ba
Mahima-Dewangan <mahimadewangan11.com>
2025-08-27 04:32:24 +0530

    register user feature created

---
+++ server/controllers/userController.js
@@ -0,0 +1,39 @@
+import User from "../modles/User.JS";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+// Register User  : /api/user/register
+export const register  = async (req , res)=>{
+    try {
+          const {name , email , password} = req.body;
+
+          if(!name || !email || !password)
+          {
+            return res.json({success : false , message: 'Missing Details'})
+          }
+
+          const existingUser =  await User.findOne({email})
+
+          if(existingUser)
+            return res.json({success : false , message: 'User already exists'}) 
+
+          const hashedPassword = await bcrypt.hash(password , 10)
+
+          const  user = await User.create({name , email , password: hashedPassword})
+
+          const token = jwt.sign({id:user._id} , ProcessingInstruction.env.JWT_SECRET , {expiresIn: '7d'});
+
+          res.cookie('token' , token , {
+             httpOnly : true ,  //[prevent JS to access the cookie]
+             secure   : process.env.NODE_ENV === 'production' , // use secure cookie in production
+             sameSite : process.env.NODE_ENV === 'production' ? 'none' : 'strict', //CSRF protection
+             maxAge  :  7 * 24 * 60 * 60 * 1000 , //cookie expiration time 
+
+          })
+
+          return res.json({success : true , user: {email : user.email , name: user.name}}) 
+
+    } catch (error) {
+        res.json({success : false , message: error.message }) ; 
+    }
+}
