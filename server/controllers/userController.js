6d16be024a844e612482d59e4270a8fa76d3448d
Mahima-Dewangan <mahimadewangan11.com>
2025-08-28 22:42:53 +0530

    user registration and login done (POST API)

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,4 @@
-import User from "../modles/User.JS";
+import User from "../modles/User.js";
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
@@ -21,7 +21,7 @@ export const register  = async (req , res)=>{
 
           const  user = await User.create({name , email , password: hashedPassword})
 
-          const token = jwt.sign({id:user._id} , ProcessingInstruction.env.JWT_SECRET , {expiresIn: '7d'});
+          const token = jwt.sign({id:user._id} , process.env.JWT_SECRET , {expiresIn: '7d'});
 
           res.cookie('token' , token , {
              httpOnly : true ,  //[prevent JS to access the cookie]
@@ -36,4 +36,48 @@ export const register  = async (req , res)=>{
     } catch (error) {
         res.json({success : false , message: error.message }) ; 
     }
+}
+
+
+// Login User :  /api/user/login
+
+export const login =  async(req , res) =>
+{
+  try {
+
+      const {email, password} = req.body;
+
+      if(!email || !password)
+        return res.json({success : false , message:'Email and password are required'});
+
+      const user = await User.findOne({email});
+
+      if(!user)
+      {
+        return res.json({success:false, message:'Invalid email or password'});
+      }
+    
+      const isMatch = await bcrypt.compare(password, user.password)
+
+      if(!isMatch)
+        return res.json({success:false , message:'Invalid email or password' });
+
+      const token = jwt.sign({id:user._id} , process.env.JWT_SECRET , {expiresIn: '7d'});
+
+          res.cookie('token' , token , {
+             httpOnly : true ,  //[prevent JS to access the cookie]
+             secure   : process.env.NODE_ENV === 'production' , // use secure cookie in production
+             sameSite : process.env.NODE_ENV === 'production' ? 'none' : 'strict', //CSRF protection
+             maxAge  :  7 * 24 * 60 * 60 * 1000 , //cookie expiration time 
+
+          })
+
+          return res.json({success : true , user: {email : user.email , name: user.name}}) 
+
+
+  } catch (error) {
+    console.log(error.message);
+      res.json({success : false , message: error.message }) ; 
+  }
+
 }
