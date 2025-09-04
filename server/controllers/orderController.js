a5d2b2324d503f51663dca3a44371b062d5fc455
Mahima-Dewangan <mahimadewangan11.com>
2025-09-04 20:23:24 +0530

    backend updated

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,6 +1,7 @@
 import Order from "../modles/Order.js";
 import Product from "../modles/Product.js";
-
+import stripe from "stripe"
+import User from "../modles/User.js"
 
 // Place Order COD : /api/order/cod
 export const placeOrderCOD = async ( req , res)=>{
@@ -34,6 +35,141 @@ export const placeOrderCOD = async ( req , res)=>{
         
         return res.json({success:false , message: error.message});
     }
+}
+
+
+
+// Place Order Stripe : /api/order/stripe
+export const placeOrderStripe = async ( req , res)=>{
+    try {
+        const {userId , items , address} = req.body;
+        const { origin } = req.headers;
+
+        if(!address || items.length ==0){
+            return res.json({success:false , message: "Invalid data"})
+        }
+
+        let productData = [];
+
+        // Calculate Amount Using Items
+        let amount = await items.reduce(async (acc , item)=>{
+            const product = await Product.findById(item.product);
+            productData.push({
+                name: product.name,
+                price: product.offerPrice , 
+                quantity: item.quantity
+            });
+            return  (await acc) + product.offerPrice * item.quantity;
+        } , 0)
+
+        // Add Tax Charge (2%)
+        amount += Math.floor(amount * 0.02);
+
+
+       const order =   await Order.create({
+            userId,
+            items,
+            amount,
+            address,
+            paymentType: "Online",
+
+        });
+
+        // Stripe Gateway Initialize
+        const StripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
+
+        // create line items for stripe 
+        const line_items= productData.map((item)=>{
+            return{
+                price_data: {
+                    currency: "inr",
+                    product_data:{
+                        name: item.name,
+                    },
+                    unit_amount: Math.floor(item.price + item.price * 0.02) * 100
+                },
+                quantity:item.quantity,
+            }
+        })
+
+        // create session
+        const session = await StripeInstance.checkout.sessions.create({
+            line_items , 
+            mode: "payment",
+            success_url:`${origin}/loader?next=my-orders` ,
+            cancel_url: `${origin}/cart`,
+            metadata: {
+                orderId : order._id.
+                userId, 
+            }
+        })
+
+        return res.json({success:true,  url: session.url})
+    } catch (error) {
+        
+        return res.json({success:false , message: error.message});
+    }
+}
+
+
+// Stripe Webhooks to verify Payments Action : /stripe
+export const stripeWebhooks = async (request , response)=>{
+    // Stripe Gateway Initialize 
+    const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
+
+    const sig= request.headers["stripe signature"];
+    let event;
+
+    try{
+        event = stripeInstance.webhooks.constructEvent(
+            request.body,
+            sig,
+            process.env.STRIPE_WEBHOOK_SECRET
+
+        );
+    } catch(error){
+        response.status(400).send(`Webhook error ${error.message}`)
+    }
+
+    // handle the event
+    switch (event.type) {
+        case "payment_intent.succeeded":{
+            const paymentIntent = event.data.object;
+            const paymentIntentId = paymentIntent.id;
+
+            // Getting session metadata
+            const session = await stripeInstance.checkout.sessions.list({
+                payment_intent:paymentIntentId,
+            });
+
+            const {orderId , userId } = session.data[0].metadata;
+
+            // Mark the payment as paid
+            await Order.findByIdAndUpdate(orderId , {isPaid: true})
+            // Clear user cart
+            await User.findByIdAndUpdate(userId , {cartItems : {}});
+            break; 
+        }
+
+        case "payment_intent.succeeded": {
+              const paymentIntent = event.data.object;
+            const paymentIntentId = paymentIntent.id;
+
+            // Getting session metadata
+            const session = await stripeInstance.checkout.sessions.list({
+                payment_intent:paymentIntentId,
+            });
+
+            const {orderId } = session.data[0].metadata;
+            await Order.findByIdAndDelete(orderId);
+            break;
+        }
+            
+        default:
+            console.error(`unhandled event type ${event.type}`)
+            break;
+    }
+    response.json({received:true});
 }
 
 
--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ import productRouter from './routes/ProductRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import addressRouter from './routes/addressRoute.js';
 import orderRouter from './routes/orderRoute.js';
+import { stripeWebhooks } from './controllers/orderController.js';
 
 
 const app = express();
@@ -21,6 +22,8 @@ await connectCloudinary()
 
 // allow multiple origins
 const allowedOrigins = ['http://localhost:5173']
+
+app.post('/stripe' , express.raw({type: 'application/json'}) , stripeWebhooks)
 
 // Middleware configuration
 app.use(express.json());
