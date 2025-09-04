3ce856cbd257d26abd2dc772129f4cef99ac13a7
Mahima-Dewangan <mahimadewangan11.com>
2025-09-04 15:15:00 +0530

    my orders page integrated with backend successfully

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -5,16 +5,30 @@ import { dummyOrders } from '../assets/assets'
 const MyOrders = () => {
 
     const [myOrders , setMyOrders] = useState([])
-    const {currency} = UseAppContext()
+    const {currency , axios , user} = UseAppContext()
 
 
     const fetchMyOrders = async () =>{
-        setMyOrders(dummyOrders)
+        try {
+            
+            const { data } = await axios.get('/api/order/user')
+            if(data.success)
+            {
+                setMyOrders(data.orders)
+            }
+
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     useEffect(()=>{
-        fetchMyOrders()
-    },[])
+        if(user)
+        {
+             fetchMyOrders()
+        }
+        
+    },[user])
 
   return (
     <div className='mt-16 pb-16'>
